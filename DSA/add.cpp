f63532fb78ec1e192f0f4d1ef6f0a71b636bb840
Rana299bhavesh <ranabhavesh299@gmail.com>
2022-10-26 13:29:04 +0530

    sortzero_one

---
+++ DSA/add.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,f,g;
+
+    cout<<"Enter the numbers "<<endl;
+    cin>>a>>b;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    g=a%b;
+    cout<<"Sum of a&b is "<<c<<endl;
+    cout<<"Sub of a&b is "<<d<<endl;
+    cout<<"multiply of a&b is "<<e<<endl;
+    cout<<"divide of a&b is "<<f<<endl;
+    cout<<"mod of a&b is "<<g<<endl;
+    
+
+
+
+}
