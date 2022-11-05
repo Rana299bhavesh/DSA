4b6c90d90b8a1b5aac9eea26982dc8a9e0dcd64c
Rana299bhavesh <ranabhavesh299@gmail.com>
2022-11-05 17:58:00 +0530

    sum

--- DSA/sum.cpp
+++ DSA/sum.cpp
@@ -11,11 +11,11 @@ int main(){
     e=a*b;
     f=a/b;
     g=a%b;
-    cout<<"Sum of a&b is "<<c<<endl;
-    cout<<"Sub of a&b is "<<d<<endl;
-    cout<<"multiply of a&b is "<<e<<endl;
-    cout<<"divide of a&b is "<<f<<endl;
-    cout<<"mod of a&b is "<<g<<endl;
+    cout<<"Sum of a & b is:- "<<c<<endl;
+    cout<<"Subtraction of a & b is:- "<<d<<endl;
+    cout<<"Multiplication of a & b is:- "<<e<<endl;
+    cout<<"Division of a & b is:- "<<f<<endl;
+    cout<<"Mod of a & b is:- "<<g<<endl;
     
 
 
