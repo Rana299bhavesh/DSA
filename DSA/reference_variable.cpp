a44ba8dc9e5a0e609f898e20bbbe5842b47b5ada
Rana299bhavesh <ranabhavesh299@gmail.com>
2022-11-30 16:46:10 +0530

    2darray

---
+++ DSA/reference_variable.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void update2(int& n){
+    n++;
+}
+
+void update(int n){
+    n++;
+}
+
+int main(){
+    /*int i=5;
+    //Create a ref variable 
+    int& j=i;
+
+    cout<< i << endl;
+    i++;
+    cout<< i << endl;
+    j++;
+    cout<< i << endl;*/
+
+    int n=5;
+    cout<< "Before" << n << endl;
+    update2(n);
+    cout<< "After" << n << endl;
+    
+    cout<< "Before" << n << endl;
+
+
+    return 0;
+}
