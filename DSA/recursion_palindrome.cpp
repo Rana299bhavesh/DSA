859f519b75c09fe1588410881cebbc060b954a6d
Rana299bhavesh <ranabhavesh299@gmail.com>
2022-12-03 13:15:00 +0530

    recursion

---
+++ DSA/recursion_palindrome.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+bool checkpalindrome(string str, int i, int j){
+
+    if(i>j){
+        return true;
+    }
+    if(str[i]!=str[j]){
+        return false;
+
+    }
+    else{
+        return checkpalindrome(str, i+1, j-1);
+    }
+
+
+
+}
+
+int main(){
+
+    string name="rana";
+    cout<<endl;
+    bool ispalindrome= checkpalindrome(name, 0, name.length()-1);
+    if(ispalindrome){
+        cout<<"PAlindrome hai "<<endl;
+
+    }
+    else{
+        cout<<"palindrome nahi hai"<< endl;
+
+    }
+
+    return 0;
+
+
+
+
+
+    
+
+
+}
